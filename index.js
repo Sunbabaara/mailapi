a6fa135c2bd2a5a477fbfe84b0912f43601ac4b9
Sunbabaara <csunbabaara@gmail.com>
2022-01-31 00:32:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express= require("express");
+const dotenv =require("dotenv");
+const connectDB= require("./config/connectDB");
+const userRoute = require("./routes/userRoute");
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+//middlewares
+app.use(express.json());
+
+//routes
+
+//home 
+app.get ("/", (req, res)=> {
+    res.send("<h1>welcome</h1>");
+
+})
+
+const port =process.env.PORT || 6000
+
+app.listen(port, ()=>
+console.log (`server is running ${port}`))
